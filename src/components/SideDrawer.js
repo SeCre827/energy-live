6c9a2872b18dcff2dce50f509fad58f86ceb47e4
SeCre827 <lefterisoikonomy@yahoo.gr>
2022-04-05 10:27:15 +0300

    Added a date picker
    Used "https://github.com/wojtekmaj/react-date-picker" component

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './SideDrawer.module.css';
+import DatePicker from 'react-date-picker';
 
 const SideDrawer = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const changeDateHandler = (date) => {
+    setStartDate(date);
+    console.log(date);
+  };
+
   return (
     <div className={classes.mainDiv}>
       <h1> EnergyLive 2022</h1>
-      <div className={classes.infoDiv}> INFO </div>
+      <div className={classes.infoDiv}>
+        <h2>Starting Date:</h2>
+        <div className={classes.picker}>
+          <DatePicker
+            className={classes.picker1}
+            onChange={changeDateHandler}
+            value={startDate}
+          />
+        </div>
+        <div>Rest buttons that change Dynamically</div>
+      </div>
     </div>
   );
 };
