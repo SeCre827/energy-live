742c38d7f2f67ce28f61e747bbd2a61c50c5820a
SeCre827 <lefterisoikonomy@yahoo.gr>
2022-04-07 18:45:41 +0300

    The sidedrawer changes dynamically based on the Quantity.

--- src/components/SideDrawer.js
+++ src/components/SideDrawer.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './SideDrawer.module.css';
 
 // να φτιαξω ενα component option που να κανει render δυναμικα τα options me map kai tha tou pernaw ta options me ena array
@@ -6,9 +6,14 @@ import classes from './SideDrawer.module.css';
 // allazei to component
 
 const SideDrawer = () => {
-  const quantityInputRef = useRef();
   const countryInputRef = useRef();
   const startDateInputRef = useRef();
+  const quantityInputRef = useRef();
+  const generationTypeInputRef = useRef();
+  const countryToInputRef = useRef();
+  const countryFromInputRef = useRef();
+
+  const [mainPage, setMainPage] = useState('actualTotalLoad');
 
   function submitHandler(event) {
     event.preventDefault();
@@ -23,7 +28,11 @@ const SideDrawer = () => {
     console.log(startDate);
   }
 
-  const changehandler = () => {};
+  const changeHandler = (event) => {
+    const newMainPage = quantityInputRef.current.value;
+    setMainPage(newMainPage);
+  };
+
   return (
     <div className={classes.mainDiv}>
       <h1> EnergyLive 2022</h1>
@@ -42,10 +51,16 @@ const SideDrawer = () => {
         </div>
         <form onSubmit={submitHandler} className={classes.form1}>
           <div className={classes.options}>
+            {/* 1nd option */}
             <div>
               <label htmlFor='quantity'>Quantity:</label>
               <br></br>
-              <select name='quantity' id='quantity' ref={quantityInputRef}>
+              <select
+                name='quantity'
+                id='quantity'
+                ref={quantityInputRef}
+                onChange={changeHandler}
+              >
                 <option value='actualTotalLoad'>Actual total Load</option>
                 <option value='generationPerType'>Generation Per Type</option>
                 <option value='crossBorderFlows'>Cross border flows</option>
@@ -54,16 +69,65 @@ const SideDrawer = () => {
               <br></br>
               <br></br>
             </div>
-            <div>
-              <label htmlFor='country'>Country:</label>
-              <br></br>
-              <select name='country' id='country' ref={countryInputRef}>
-                <option value='a'>A</option>
-                <option value='b'>B</option>
-                <option value='c'>C</option>
-                <option value='d'>D</option>
-              </select>
-            </div>
+            {/* 2nd option */}
+            {(mainPage === 'actualTotalLoad' ||
+              mainPage === 'generationPerType') && (
+              <div>
+                <label htmlFor='country'>Country:</label>
+                <br></br>
+                <select name='country' id='country' ref={countryInputRef}>
+                  <option value='a'>A</option>
+                  <option value='b'>B</option>
+                  <option value='c'>C</option>
+                  <option value='d'>D</option>
+                </select>
+              </div>
+            )}
+            {mainPage === 'crossBorderFlows' && (
+              <div>
+                <label htmlFor='countryFrom'>Country (from):</label>
+                <br></br>
+                <select
+                  name='countryFrom'
+                  id='countryFrom'
+                  ref={countryFromInputRef}
+                >
+                  <option value='a'>A</option>
+                  <option value='b'>B</option>
+                  <option value='c'>C</option>
+                  <option value='d'>D</option>
+                </select>
+              </div>
+            )}
+            {/* end of second option  */}
+            {/* start of 3 option */}
+            {mainPage === 'generationPerType' && (
+              <div>
+                <label htmlFor='generationType'>Generation Type:</label>
+                <br></br>
+                <select
+                  name='generationType'
+                  id='generationType'
+                  ref={generationTypeInputRef}
+                >
+                  <option value='naturalGas'>Nautral gas</option>
+                  <option value='oil'>Oil</option>
+                </select>
+              </div>
+            )}
+            {mainPage === 'crossBorderFlows' && (
+              <div>
+                <label htmlFor='countryTo'>Country (to):</label>
+                <br></br>
+                <select name='countryTo' id='countryTo' ref={countryToInputRef}>
+                  <option value='a'>A</option>
+                  <option value='b'>B</option>
+                  <option value='c'>C</option>
+                  <option value='d'>D</option>
+                </select>
+              </div>
+            )}
+            {/* end  of 3rd option */}
           </div>
           <div className={classes.submitButton}>
             <button>Refresh</button>
